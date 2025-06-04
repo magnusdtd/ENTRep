557f8223e8e430d5177aed80e3cc58d14b28c07f
Dam Tien Dat <tdat0306@gmail.com>
2025-06-04 11:28:50 +0700

    Remove  make_path.py

--- utils/file.py
+++ utils/file.py
@@ -49,3 +49,24 @@ class File:
             raise ValueError(f"The path {directory_path} is not a directory or is a symlink")
 
         shutil.rmtree(directory)
+
+    @staticmethod
+    def make_path():
+        """This function will create paths to run the notebook in Kaggle"""
+
+        # First, run the EDA.ipynb to create a cleaned dataset
+        os.system('jupyter nbconvert --to notebook --execute ../EDA/EDA.ipynb --inplace')
+
+        # Copy all repo files to the current directory
+        File.copy_files(
+        "/kaggle/input/ENTRep",
+        "/kaggle/working/"
+        )
+
+        # Copy the Kaggle dataset to the current directoru
+        File.copy_files(
+            "/kaggle/input/entrep-public-dataset",
+            "/kaggle/working/Dataset"
+        )
+
+  
