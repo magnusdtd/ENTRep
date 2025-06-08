caad325f2353174aaec310b1da8d20f1a653fcb7
Dam Tien Dat <tdat0306@gmail.com>
2025-06-08 15:22:03 +0700

    Adopt the the code base of ResNet to use train dataset

--- resnet/evaluate.py
+++ resnet/evaluate.py
@@ -15,10 +15,11 @@ def evaluate_class_model(model, val_loader:DataLoader, class_feature_map:dict):
     with torch.no_grad():
         for images, labels in val_loader:
             images = images.to(device)
-            labels = labels['class'].to(device)
+            labels = labels['class']
+            labels = labels.to(device)
 
-            classification_output, _ = model.forward(images)
-            preds = torch.argmax(classification_output, dim=1).cpu().numpy()
+            outputs = model.forward(images)
+            preds = torch.argmax(outputs, dim=1).cpu().numpy()
             all_preds.extend(preds)
             all_labels.extend(labels.cpu().numpy())
 
@@ -34,34 +35,3 @@ def evaluate_class_model(model, val_loader:DataLoader, class_feature_map:dict):
 
     print("Classification Report:")
     print(classification_report(all_labels, all_preds, target_names=class_names))
-
-def evaluate_type_model(model, val_loader: DataLoader, type_feature_map: dict):
-    """Evaluate the model on the validation dataset and plot a confusion matrix heatmap for type predictions."""
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.model.eval()
-
-    all_preds = []
-    all_labels = []
-
-    with torch.no_grad():
-        for images, labels in val_loader:
-            images = images.to(device)
-            labels = labels['type'].to(device)
-
-            _, type_output = model.forward(images)
-            preds = torch.argmax(type_output, dim=1).cpu().numpy()
-            all_preds.extend(preds)
-            all_labels.extend(labels.cpu().numpy())
-
-    cm = confusion_matrix(all_labels, all_preds, labels=list(type_feature_map.values()))
-    type_names = list(type_feature_map.keys())
-
-    plt.figure(figsize=(10, 8))
-    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=type_names, yticklabels=type_names)
-    plt.xlabel('Predicted Labels')
-    plt.ylabel('True Labels')
-    plt.title('Confusion Matrix Heatmap for Type Predictions')
-    plt.show()
-
-    print("Classification Report for Type Predictions:")
-    print(classification_report(all_labels, all_preds, labels=list(type_feature_map.values()), target_names=type_names))
--- resnet/inference.py
+++ resnet/inference.py
@@ -3,20 +3,19 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from PIL import Image
 import numpy as np
-import torchvision.models as models
-from resnet.resnet import Resnet
+from ResNet.resnet import ResNet
 import os, random
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def load_model(model_path, device):
-    """Load the trained model state dictionary into the Resnet architecture."""
-    model = Resnet(backbone=models.resnet50(weights=models.ResNet50_Weights.DEFAULT))
+def load_model(model_path:str, backbone, device:str):
+    """Load the trained model state dictionary into the ResNet architecture."""
+    model = ResNet(backbone=backbone)
     model.model.load_state_dict(torch.load(model_path, map_location=device))
     model.model.eval()
     return model
 
-def preprocess_image(image_path):
+def preprocess_image(image_path:str):
     """Preprocess the image for inference using Albumentations."""
     transform = A.Compose([
         A.Resize(224, 224),
@@ -31,25 +30,22 @@ def preprocess_image(image_path):
 def classify_image(model, image_tensor, device):
     """Classify the image using the model."""
     image_tensor = image_tensor.to(device)
-    classification_output, type_output = model.forward(image_tensor)
+    outputs = model.forward(image_tensor)
 
-    classification_prediction = torch.argmax(classification_output, dim=1).item()
-    type_prediction = torch.argmax(type_output, dim=1).item()
+    classification_prediction = torch.argmax(outputs, dim=1).item()
 
-    return classification_prediction, type_prediction
+    return classification_prediction
 
 def random_inference_9_images(
     model, 
     df:pd.DataFrame, 
     class_feature_map:dict, 
-    type_feature_map:dict, 
     device: str,
-    image_folder: str = "Dataset/images"
+    image_folder: str = "Dataset/train/imgs"
 ):
     """Randomly select 9 images from the folder"""
 
     inv_class_feature_map = {v: k for k, v in class_feature_map.items()}
-    inv_type_feature_map = {v: k for k, v in type_feature_map.items()}
     image_files = os.listdir(image_folder)
     selected_images = random.sample(image_files, 9)
 
@@ -58,15 +54,18 @@ def random_inference_9_images(
         image_path = os.path.join(image_folder, selected_images[i])
         image = Image.open(image_path).convert("RGB")
         image_name = os.path.basename(image_path)
-        true_class = df[df['Path'] == image_name]['Classification'].values[0]
-        true_type = df[df['Path'] == image_name]['Type'].values[0]
+        true_class = df[df['Path'] == image_name]['Classification']
+        if true_class.empty:
+            print(f"Warning: No matching entry found for {image_name} in DataFrame.")
+            continue
+        true_class = true_class.values[0]
         image_tensor = preprocess_image(image_path)
-        class_, type_ = classify_image(model, image_tensor, device)
+        class_ = classify_image(model, image_tensor, device)
 
         ax.imshow(image)
         ax.axis('off')
         ax.set_title(
-            f"True: {true_class}/{true_type}\nPred: {inv_class_feature_map[class_]}/{inv_type_feature_map[type_]}",
+            f"True: {true_class}\nPred: {inv_class_feature_map[class_]}",
         )
         fig.suptitle("True Labels and Predictions for Random Images", fontsize=18, y=1.03)
     plt.tight_layout(rect=[0, 0, 1, 0.97])
@@ -86,7 +85,6 @@ if __name__ == "__main__":
     model = load_model(args.model_path, device)
     image_tensor = preprocess_image(args.image_path)
 
-    classification, type_ = classify_image(model, image_tensor, device)
+    classification = classify_image(model, image_tensor, device)
 
     print(f"Classification Prediction: {classification}")
-    print(f"Type Prediction: {type_}")
