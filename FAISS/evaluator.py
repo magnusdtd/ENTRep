456e76068a3ec75655072ed47fd6035eeb9a666b
Dam Tien Dat <tdat0306@gmail.com>
2025-06-09 20:26:58 +0700

    Handle ZeroDivisionError issue in evaluator.py

--- FAISS/evaluator.py
+++ FAISS/evaluator.py
@@ -2,14 +2,14 @@ import numpy as np
 
 class Evaluator:
   @staticmethod
-  def evaluate_recall_at_k(index, features, labels, paths, K_values=[1, 5, 10]):
+  def evaluate_recall_at_k(index, features, labels, K_values=[1, 5, 10]):
     recalls = {K: [] for K in K_values}
     total_per_class = {}
 
     for label in labels:
       total_per_class[label] = total_per_class.get(label, 0) + 1
 
-    D, I = index.search(features, max(K_values) + 1)
+    _, I = index.search(features, max(K_values) + 1)
     for i, query_lbl in enumerate(labels):
       retrieved_idxs = I[i].tolist()
       if retrieved_idxs[0] == i:
@@ -20,7 +20,11 @@ class Evaluator:
       for K in K_values:
         topk = retrieved_idxs[:K]
         rel = sum(1 for idx in topk if labels[idx] == query_lbl)
-        recall = rel / (total_per_class[query_lbl] - 1)
+        denominator = total_per_class[query_lbl] - 1
+        if denominator == 0:
+            recall = 0
+        else:
+            recall = rel / denominator
         recalls[K].append(recall)
 
     avg_recalls = {K: np.mean(recalls[K]) for K in K_values}
