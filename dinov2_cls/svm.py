e48677a0d2b1c1c13b119e172052e4a7e729c76d
Dam Tien Dat <tdat0306@gmail.com>
2025-06-17 13:39:46 +0700

    Implement submission functionality in SVM class to save predictions as JSON and create a ZIP archive

--- dinov2_cls/svm.py
+++ dinov2_cls/svm.py
@@ -1,15 +1,19 @@
-from typing import List, Callable
+from typing import List, Callable, Dict
 from sklearn import svm
 import numpy as np
 import torch
 import cv2
 from PIL import Image
+import json
+import zipfile
+import datetime
+import os
 
 class SVM:
     def __init__(self, backbone, embedding: List, y: List, transform: Callable):
         self.clf = svm.SVC(gamma='scale')
         self.clf.fit(np.array(embedding).reshape(-1, 384), y)
-        self.device = "cuda" if torch.is_available() else "cpu"
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.backbone = backbone
         self.transform = transform
 
@@ -37,3 +41,39 @@ class SVM:
         plt.show()
 
         print("\nPredicted class: " + prediction[0])
+    
+    def make_submission(
+            self, 
+            test_file_path:str, 
+            img_folder_path:str, 
+            label_map: Dict[str, int],
+            output_folder_path: str = './results'
+        ):
+        test_df = pd.read_csv(test_file_path, header=None, names=['Path'])
+        predictions = {}
+        for img_name in test_df['Path']:
+            img_path = os.path.join(img_folder_path, img_name)
+            img = self.load_img(img_path)
+
+            with torch.no_grad():
+                embedding = self.backbone(img.to(self.device))
+
+            prediction = self.clf.predict(np.array(embedding[0].cpu()).reshape(1, -1))
+            predictions[img_name] = label_map[prediction[0]]
+        
+        # Generate unique JSON filename with model_name as prefix
+        daytime = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+        json_file_name = f'DINOv2_SVM_{daytime}.json'
+        json_file_path = os.path.join(output_folder_path, json_file_name)
+
+        # Save predictions to JSON file
+        with open(json_file_path, 'w') as json_file:
+            json.dump(predictions, json_file)
+
+        # Create ZIP archive with the same name as the JSON file
+        zip_file_path = os.path.join(output_folder_path, f'DINOv2_SVM_{daytime}.zip')
+        with zipfile.ZipFile(zip_file_path, 'w') as zip_file:
+            zip_file.write(json_file_path, arcname=json_file_name)
+
+        print(f"Submission file created at: {zip_file_path}")
+            
