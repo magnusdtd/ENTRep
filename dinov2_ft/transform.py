8434c417517728fc75b39cc816996a06f7c6c53f
Dam Tien Dat <tdat0306@gmail.com>
2025-06-20 15:08:21 +0700

    Rename folder; Add  DINOv2 + BERT for t2i retrieval

---
+++ dinov2_ft/transform.py
@@ -0,0 +1,10 @@
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+train_transform = A.Compose([
+    A.RandomResizedCrop(224, 224, scale=(0.8, 1.0)),
+    A.HorizontalFlip(p=0.5),
+    A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1, p=0.5),
+    A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ToTensorV2(),
+])
