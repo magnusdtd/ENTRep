c14829be63dc0eff7f2cad6b9450938033534aa9
Dam Tien Dat <tdat0306@gmail.com>
2025-06-25 20:51:28 +0700

    Add DINOv2 for i2i task

---
+++ FAISS/DINOv2.py
@@ -0,0 +1,59 @@
+from FAISS.feature_extractor import FeatureExtractor
+import torch
+from PIL import Image
+import open_clip
+import numpy as np
+import os
+from typing import Callable
+
+class DINOv2_FE(FeatureExtractor):
+  def __init__(
+      self, 
+      repo_name: str = 'facebookresearch/dinov2', 
+      model_name:str='dinov2_vits14', 
+      img_folder_path:str = 'Dataset/train/imgs',
+      transform: Callable = None,
+    ):
+    super().__init__()
+    self.model = torch.hub.load(repo_name, model_name)
+    self.model.to(self.device)
+    self.model.eval()
+    self.img_folder_path = img_folder_path
+    self.transform = transform
+
+  def load_img(self, img_path:str) -> torch.Tensor:
+    img = Image.open(img_path)
+
+    transformed_img = self.transform(img)[:3].unsqueeze(0)
+
+    return transformed_img
+
+  def extract_features(self, dataloader, is_inference: bool = False):
+    all_features = []
+    all_labels = []
+    all_paths = []
+    
+    with torch.no_grad():
+      for batch in dataloader:
+        if is_inference:
+          _, img_names = batch
+          labels = None
+        else:
+          _, labels, img_names = batch
+
+        for img_name in img_names:
+          img_path = os.path.join(self.img_folder_path, img_name)
+
+          output = self.model(self.load_img(img_path).to(self.device))
+
+          all_features.append(np.array(output[0].cpu().numpy()).reshape(1, -1).tolist())
+          
+        if not is_inference:
+          all_labels.extend(labels)
+        all_paths.extend(img_names)
+
+      features = np.vstack(all_features).astype('float32')
+      if is_inference:
+        return features, all_paths
+      else:
+        return features, all_labels, all_paths
