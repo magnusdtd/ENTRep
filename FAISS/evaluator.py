e0b7acc0c90eda0014a52e2632eeaab0c1fc3910
Dam Tien Dat <tdat0306@gmail.com>
2025-06-29 13:06:32 +0700

    Update Recall@K calculation

--- FAISS/evaluator.py
+++ FAISS/evaluator.py
@@ -4,14 +4,14 @@ class Evaluator:
   @staticmethod
   def _recall_at_k(query_labels: np.array, gallery_labels: np.array, topk_indices: np.array, k: int):
     """
-    Calculate Recall@K for a retrieval system.
-    
+    Calculate standard Recall@K for a retrieval system. Standard Recall@K is defined as the proportion of queries for which at least one relevant item (i.e., with the same label as the query) appears in the top-K retrieved results.
+
     Parameters:
         query_labels (np.ndarray): Labels of query images, shape (n_queries,)
         gallery_labels (np.ndarray): Labels of gallery images, shape (n_gallery,)
         topk_indices (np.ndarray): Indices of top-K gallery results for each query, shape (n_queries, K)
         k (int): Value of K for Recall@K
-    
+
     Returns:
         float: Recall@K score
     """
@@ -19,18 +19,16 @@ class Evaluator:
     gallery_labels = np.asarray(gallery_labels)
     topk_indices = np.asarray(topk_indices)[:, :k]
 
-    correct_retrievals = 0
+    num_queries = len(query_labels)
+    num_correct = 0
 
     for i, query_label in enumerate(query_labels):
       # Top-k retrieved labels for this query
       retrieved_labels = gallery_labels[topk_indices[i]]
-      num_relevant_in_topk = np.sum(retrieved_labels == query_label)
+      if np.any(retrieved_labels == query_label):
+        num_correct += 1
 
-      # Recall for this query: relevant@k / total images in query
-      recall = num_relevant_in_topk / k
-      correct_retrievals += recall
-
-    return correct_retrievals / len(query_labels)
+    return num_correct / num_queries
 
   @staticmethod
   def evaluate_recall_at_k(index, features, query_labels, K_values=[1, 5, 10]):
