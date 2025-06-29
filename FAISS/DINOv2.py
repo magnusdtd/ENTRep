2860bb97d069bc10b003b3209e73e8f0656105d7
Dam Tien Dat <tdat0306@gmail.com>
2025-06-29 13:26:47 +0700

    Update DINOv2 as a feature extractor for FAISS usage

--- FAISS/DINOv2.py
+++ FAISS/DINOv2.py
@@ -1,31 +1,40 @@
 from FAISS.feature_extractor import FeatureExtractor
 import torch
 from PIL import Image
-import open_clip
 import numpy as np
 import os
-from typing import Callable
+from typing import Tuple
+import torchvision.transforms as T
 
 class DINOv2_FE(FeatureExtractor):
   def __init__(
       self, 
       repo_name: str = 'facebookresearch/dinov2', 
-      model_name:str='dinov2_vits14', 
+      model_name:str='dinov2_vits14',
       img_folder_path:str = 'Dataset/train/imgs',
-      transform: Callable = None,
+      image_size: Tuple[int, int] = (490, 644)
     ):
     super().__init__()
     self.model = torch.hub.load(repo_name, model_name)
     self.model.to(self.device)
     self.model.eval()
     self.img_folder_path = img_folder_path
-    self.transform = transform
+    self.image_size = image_size
+    
+    self.preprocess = T.Compose([
+        T.Resize(image_size, interpolation=Image.BICUBIC),
+        T.ToTensor(),
+        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
 
   def load_img(self, img_path:str) -> torch.Tensor:
     img = Image.open(img_path)
-
-    transformed_img = self.transform(img)[:3].unsqueeze(0)
-
+    
+    if img.mode != 'RGB':
+      img = img.convert('RGB')
+    
+    transformed_img = self.preprocess(img).unsqueeze(0)
+    
     return transformed_img
 
   def extract_features(self, dataloader, is_inference: bool = False):
@@ -57,3 +66,11 @@ class DINOv2_FE(FeatureExtractor):
         return features, all_paths
       else:
         return features, all_labels, all_paths
+
+  def extract_feature(self, img_path: str):
+    """
+    Extract feature from a single image at img_path
+    """
+    with torch.no_grad():
+      output = self.model(self.load_img(img_path).to(self.device))
+      return output[0].cpu().numpy() 
