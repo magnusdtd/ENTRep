13e2377b26efd171427df77451be23e7abb29814
Dam Tien Dat <tdat0306@gmail.com>
2025-06-29 14:17:55 +0700

    Update BioCLIP

--- FAISS/BioCLIP.py
+++ FAISS/BioCLIP.py
@@ -10,7 +10,6 @@ class BioCLIP_FE(FeatureExtractor):
       self, 
       model_name: str, 
       model_path:str="", 
-      img_folder_path:str = 'Dataset/train/imgs'
     ):
     super().__init__()
     if model_path:
@@ -19,7 +18,6 @@ class BioCLIP_FE(FeatureExtractor):
       self.model, _, self.preprocess_val = open_clip.create_model_and_transforms(model_name)
     self.model.to(self.device)
     self.model.eval()
-    self.img_folder_path = img_folder_path
 
   def extract_features(self, dataloader, is_inference: bool = False):
     all_features = []
@@ -29,13 +27,12 @@ class BioCLIP_FE(FeatureExtractor):
     with torch.no_grad():
       for batch in dataloader:
         if is_inference:
-          _, img_names = batch
+          _, img_paths = batch
           labels = None
         else:
-          _, labels, img_names = batch
+          _, labels, img_paths = batch
 
-        for img_name in img_names:
-          img_path = os.path.join(self.img_folder_path, img_name)
+        for img_path in img_paths:
           img = Image.open(img_path)
 
           image_tensor = self.preprocess_val(img).unsqueeze(0).to(self.device)
@@ -46,10 +43,21 @@ class BioCLIP_FE(FeatureExtractor):
           all_features.append(image_feature.cpu().numpy())
         if not is_inference:
           all_labels.extend(labels)
-        all_paths.extend(img_names)
+        all_paths.extend(img_paths)
 
       features = np.vstack(all_features).astype('float32')
       if is_inference:
         return features, all_paths
       else:
         return features, all_labels, all_paths
+
+  def extract_feature(self, img_path: str):
+    """
+    Extract feature from a single image at img_path
+    """
+    with torch.no_grad():
+      img = Image.open(img_path)
+      image_tensor = self.preprocess_val(img).unsqueeze(0).to(self.device)
+      image_feature = self.model.encode_image(image_tensor)
+      image_feature /= image_feature.norm(dim=-1, keepdim=True)
+      return image_feature.cpu().numpy()
